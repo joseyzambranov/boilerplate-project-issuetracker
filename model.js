6f8fafe3df0e027bdb7717f32ea81870ed849f1c
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-20 23:20:08 -0500

    added filter but has bug witch boolean

--- model.js
+++ model.js
@@ -5,7 +5,7 @@ const IssueShema = new Schema({
     issue_title:{type:String , required:true},
     issue_text:{type:String,required:true},
     created_on:Date,
-    update_on:Date,
+    updated_on:Date,
     created_by:{type:String,required:true},
     assigned_to:String,
     open:Boolean,
--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,45 @@
 const mongoose = require("mongoose");
 const IssueModel = require("../model").Issue;
 const ProjectModel = require("../model").Project;
+const ObjectId = mongoose.Types.ObjectId;
+const Boolean = mongoose.Types.Boolean;
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
   
     .get(function (req, res){
-      let project = req.params.project;
-      
+      let projectName = req.params.project;
+      const {
+        _id,
+        open,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+      } = req.query;
+
+      console.log(open)
+
+      ProjectModel.aggregate([
+        {$match:{name:projectName}},
+        {$unwind:"$issues"},
+        _id!=undefined?{$match:{"issues._id":ObjectId(_id)}}:{$match:{}},
+        open!=undefined?{$match:{"issues.open": open }}:{$match:{}},
+        issue_title!=undefined?{$match:{"issues.issue_title":issue_title}}:{$match:{}},
+        issue_text!=undefined?{$match:{"issues.issue_text":issue_text}}:{$match:{}},
+        created_by!=undefined?{$match:{"issues.created_by":created_by}}:{$match:{}},
+        assigned_to!=undefined?{$match:{"issues.assigned_to":assigned_to}}:{$match:{}},
+        status_text!=undefined?{$match:{"issues.status_text":status_text}}:{$match:{}},
+      ]).exec((err,data)=>{
+        if(!data){
+          res.json([]);
+        }else{
+         let mappedData = data.map((item)=>item.issues);
+         res.json(mappedData)
+        }
+      })
     })
     
     .post(function (req, res){
