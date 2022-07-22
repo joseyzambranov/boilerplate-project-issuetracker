b8672b5e9b7e4cd7ceba718b71ce74f143fa1077
joseyzambranov <joseyzambranovpe@gmail.com>
2022-07-22 18:26:46 -0500

    added delete

--- routes/api.js
+++ routes/api.js
@@ -150,6 +150,30 @@ module.exports = function (app) {
     
     .delete(function (req, res){
       let project = req.params.project;
+      const{_id} = req.body
+      if(!_id){
+        res.json({error:"missing _id"})
+      }
+      ProjectModel.findOne({name:project},(err,projectdata)=>{
+        if(!projectdata||err){
+          res.send({error:"could not delete",_id:_id})
+        }else{
+          const issueData=projectdata.issues.id(_id)
+          if(!issueData){
+            res.json({error:"could not delete",_id:_id})
+            return
+          }
+        issueData.remove();  
+
+        projectdata.save((err,data)=>{
+          if(err||!data){
+            res.json({error:"could not delete",_id:issueData._id})
+          }else{
+            res.json({result:"successfully delete",_id:issueData._id})
+          }
+        })
+        }
+      })
       
     });
     
